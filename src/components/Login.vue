601bafd8370de8b68671bfe90171c72450434dc4
yangdinghao <995537600@qq.com>
2018-10-20 12:32:17 +0800

    试玩

---
+++ src/components/Login.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+      <van-nav-bar
+        title="登录"
+      />
+      <div class="userinfo">
+         <img class="userinfo-avatar" src="" />
+    </div>
+  </div>
+</template>
+
+<script>
+import { NavBar} from 'vant';
+import Vue from 'vue';
+
+Vue.use(NavBar);
+
+
+export default{
+    methods: {
+
+    }
+}
+</script>
+
+<style scoped>
+.userinfo {
+  margin-top: 50px;
+  margin-bottom: 50px;
+  text-align: center;
+}
+
+.userinfo-avatar {
+  width: 75px;
+  height: 75px;
+  margin: 10px;
+  border-radius: 50%;
+  border:1px solid #000;
+}
+</style>
+
+
+
